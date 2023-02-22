138651d1961409302f0a3d1267a1a7d9941cd03b
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2023-02-22 11:33:49 -0500

    commit tree 2547de6893352f0dd99bad24e5e5672f90b25f8f

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,22 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant, only: [:id, :name, :image, :price, :is_in_stock]
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else 
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
